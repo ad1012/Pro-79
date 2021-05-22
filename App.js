3a1723bdb0defa0ae82fc2063d5917b243618b0d
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-05-22 18:59:00 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
+import LoginScreen from './screens/LoginScreen';
+import AppDrawerNavigator from './components/AppDrawerNavigator'
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  LoginScreen:{screen:LoginScreen},
+  Drawer:{screen: AppDrawerNavigator}
+})
+
+const AppContainer = createAppContainer(switchNavigator)
